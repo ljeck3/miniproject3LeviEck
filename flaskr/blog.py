d686b4d0b540e46d9ff4f5b77c28dda9c540bf3c
levieck <ljeck3@mail.fhsu.edu>
2025-03-21 21:45:43 -0600

    Fixed some errors. Game Title and Publisher work now.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,7 +13,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, publisher, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -26,10 +26,9 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         publisher = request.form['publisher']
-        release = request.form['release']
-        art = request.form['art']
+        #release = request.form['release']
+        #art = request.form['art']
 
-        body = request.form['body']
         error = None
 
         if not title:
@@ -40,9 +39,9 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
+                'INSERT INTO post (title, publisher, author_id)'
                 ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                (title, publisher, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -52,7 +51,7 @@ def create():
 #UPDATE
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, publisher, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -74,7 +73,7 @@ def update(id):
 
     if request.method == 'POST':
         title = request.form['title']
-        body = request.form['body']
+        publisher = request.form['publisher']
         error = None
 
         if not title:
@@ -85,9 +84,9 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE post SET title = ?, publisher = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (title, publisher, id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
