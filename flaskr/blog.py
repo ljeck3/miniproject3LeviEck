23937cba8f9e726e4b64b43d2cb953b2257fdcb4
levieck <ljeck3@mail.fhsu.edu>
2025-03-23 23:24:47 -0500

    Bootstrap for delete option.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -4,6 +4,9 @@ Assignment: Mini Project 3
 I,     Levi Eck    , affirm that the work submitted for this assignment is entirely my own. I have not engaged in any form of academic dishonesty, including but not limited to cheating, plagiarism, or the use of unauthorized materials. I have neither provided nor received unauthorized assistance and have accurately cited all sources in adherence to academic standards. I understand that failing to comply with this integrity statement may result in consequences, including disciplinary actions as determined by my course instructor and outlined in institutional policies. By signing this statement, I acknowledge my commitment to upholding the principles of academic integrity.
 '''
 
+### INF601 - Advanced Programming in Python
+### Levi Eck
+### Mini Project 3
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
